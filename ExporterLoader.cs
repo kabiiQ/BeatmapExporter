4d0c50e6a6e411d7167945f3dd88e25a309d6a4a
kabii <jackyd.1018@live.com>
2022-06-21 19:55:37 -0500

    add version checker

--- ExporterLoader.cs
+++ ExporterLoader.cs
@@ -4,8 +4,24 @@ namespace BeatmapExporter
 {
     internal class ExporterLoader
     {
-        static void Main(string[] args)
+        const string Version = "1.1";
+        static async Task Main(string[] args) 
         {
+            // check application version 
+            try
+            {
+                var client = new HttpClient()
+                {
+                    Timeout = TimeSpan.FromSeconds(2),
+                };
+                var latest = await client.GetStringAsync("https://raw.githubusercontent.com/kabiiQ/BeatmapExporter/main/VERSION");
+                if(latest != Version)
+                {
+                    Console.WriteLine($"UPDATE AVAILABLE for BeatmapExporter: ({Version} -> {latest})\nhttps://github.com/kabiiQ/BeatmapExporter/releases/latest\n");
+                }
+            }
+            catch (Exception) { } // unable to load version from github. not critical error, dont bother user
+
             // currently only load lazer, can add interface for selecting osu stable here later
             BeatmapExporter exporter = LazerLoader.Load(args.FirstOrDefault());
             exporter.StartApplicationLoop();

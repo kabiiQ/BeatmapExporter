7888da1952d2fc5ff65131a4be32d3facaf3fd71
kabii <jackyd.1018@live.com>
2023-06-09 14:01:48 -0500

    bump to schema version 28 (no changes needed)

--- Exporters/Lazer/LazerDB/LazerDatabase.cs
+++ Exporters/Lazer/LazerDB/LazerDatabase.cs
@@ -6,7 +6,7 @@ namespace BeatmapExporter.Exporters.Lazer.LazerDB
 {
     public class LazerDatabase
     {
-        const int LazerSchemaVersion = 27;
+        const int LazerSchemaVersion = 28;
         readonly string database;
         readonly string filesDirectory;
 

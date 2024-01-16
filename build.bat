0ff71aaeb28c477094c588fab89d6fa302a1fc6d
kabii <jackyd.1018@live.com>
2024-01-16 02:42:18 -0600

    build script revision - typo

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ echo f | xcopy /Y "BeatmapExporterCLI\bin\Release\net6.0\win-x64\publish\Beatmap
 echo f | xcopy /Y "BeatmapExporterCLI\bin\Release\net6.0\osx-x64\publish\BeatmapExporterCLI" "BuildOutput\osx-BeatmapExporterCLI"
 echo f | xcopy /Y "BeatmapExporterCLI\bin\Release\net6.0\linux-x64\publish\BeatmapExporterCLI" "BuildOutput\linux-BeatmapExporterCLI"
 echo f | xcopy /Y "BeatmapExporterGUI.Desktop\bin\Release\net6.0\win-x64\publish\BeatmapExporterGUI.Desktop.exe" "BuildOutput\BeatmapExporter.exe"
-echo f | xcopy /Y "BeatmapExporterGUI.Desktop\bin\Release\net6.0\osx-64\publish\BeatmapExporterGUI.Desktop" "BuildOutput\osx-BeatmapExporter"
+echo f | xcopy /Y "BeatmapExporterGUI.Desktop\bin\Release\net6.0\osx-x64\publish\BeatmapExporterGUI.Desktop" "BuildOutput\osx-BeatmapExporter"
 echo f | xcopy /Y "BeatmapExporterGUI.Desktop\bin\Release\net6.0\linux-x64\publish\BeatmapExporterGUI.Desktop" "BuildOutput\linux-BeatmapExporter"
 
 pause

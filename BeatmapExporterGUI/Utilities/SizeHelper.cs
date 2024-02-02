31f0624e15efe018fd49411bf493c03f288e59c8
kabii <jackyd.1018@live.com>
2024-02-01 19:51:04 -0600

    add util for semi-dynamic list scrolls based on window size

---
+++ BeatmapExporterGUI/Utilities/SizeHelper.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+
+namespace BeatmapExporterGUI.Utilities
+{
+    internal class SizeHelper
+    {
+        public static double RemainingHeight(int reserved) => (App.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow?.ClientSize.Height - reserved ?? 1024;
+    }
+}

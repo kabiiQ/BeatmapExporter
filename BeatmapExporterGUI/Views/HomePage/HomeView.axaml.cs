18bf873f549e8fa85ba59d5f555e13ec1141662c
kabii <1054738+kabiiQ@users.noreply.github.com>
2025-03-21 13:18:27 -0500

    slightly decrease size of system messages panel

--- BeatmapExporterGUI/Views/HomePage/HomeView.axaml.cs
+++ BeatmapExporterGUI/Views/HomePage/HomeView.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Threading;
 using BeatmapExporterGUI.Utilities;
+using BeatmapExporterGUI.ViewModels.HomePage;
 
 namespace BeatmapExporterGUI.Views.HomePage;
 
@@ -10,7 +11,7 @@ public partial class HomeView : UserControl
     public HomeView()
     {
         InitializeComponent();
-        SystemMessages.Height = SizeHelper.RemainingHeight(350);
+        SystemMessages.Height = SizeHelper.RemainingHeight(400);
 
         // Scroll to bottom when system messages are added
         ItemsControl.ItemCountProperty.Changed.AddClassHandler<ItemsControl>((sender, e) =>
